365c9291118d561fb2c622974b5eb81df6ef4e01
gaurav700 <jangidg0786@gmail.com>
2025-08-27 14:03:23 -0400

    feat: creating UDP client and Server using Datagram sockets

---
+++ SimpleServer/src/client/UDPAudioClient.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPAudioClient {
+    private static final int PORT = 5000;
+    private static final int PACKET_SIZE = 1024;
+
+    public static void main(String[] args){
+        try(DatagramSocket clientSocket = new DatagramSocket()){
+            byte[] audioFileName = "AudioClip.wav".getBytes();
+            DatagramPacket packet = new DatagramPacket(audioFileName, audioFileName.length, InetAddress.getLocalHost(), PORT);
+            clientSocket.send(packet);
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
