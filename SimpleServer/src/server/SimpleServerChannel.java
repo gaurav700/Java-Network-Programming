819f87b70e354c363b22f3a288e13ba92e4012cf
gaurav700 <jangidg0786@gmail.com>
2025-08-27 13:07:18 -0400

    feat: implementing efficent polling

--- SimpleServer/src/server/SimpleServerChannel.java
+++ SimpleServer/src/server/SimpleServerChannel.java
@@ -5,34 +5,51 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleServerChannel {
     public static void main(String[] args){
 
+
+
         try(ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()){
             serverSocketChannel.socket().bind(new InetSocketAddress(5000));
+            serverSocketChannel.configureBlocking(false);
 
             System.out.println("Server is listening on port "+serverSocketChannel.socket().getLocalPort());
 
-            while(true){
+            List<SocketChannel> clientChannels = new ArrayList<>();
+
+            while(true) {
                 SocketChannel clientChannel = serverSocketChannel.accept();
-                System.out.printf("Client %s connected%n", clientChannel.socket().getRemoteSocketAddress());
+
+                if(clientChannel!=null){
+                    clientChannel.configureBlocking(false);
+                    clientChannels.add(clientChannel);
+                    System.out.printf("Client %s connected%n", clientChannel.socket().getRemoteSocketAddress());
+                }
 
                 ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                SocketChannel channel = clientChannel;
 
-                int readBytes = channel.read(byteBuffer);
-                if(readBytes>0){
-                    byteBuffer.flip();
-                    channel.write(ByteBuffer.wrap("Echo from server: ".getBytes()));
-                    while(byteBuffer.hasRemaining()){
-                        channel.write(byteBuffer);
+
+                for (int i = 0; i < clientChannels.size(); i++) {
+                    SocketChannel channel = clientChannels.get(i);
+                    int readBytes = channel.read(byteBuffer);
+
+                    if (readBytes > 0) {
+                        byteBuffer.flip();
+                        channel.write(ByteBuffer.wrap("Echo from server: ".getBytes()));
+                        while (byteBuffer.hasRemaining()) {
+                            channel.write(byteBuffer);
+                        }
+                        byteBuffer.clear();
+                    } else if (readBytes == -1) {
+                        System.out.printf("Connection to %s lost%n", channel.socket().getRemoteSocketAddress());
+                        channel.close();
+                        clientChannels.remove(i);
                     }
-                    byteBuffer.clear();
-                }
-                else if(readBytes == -1){
-                    System.out.printf("Connection to %s lost%n", channel.socket().getRemoteSocketAddress());
-                    channel.close();
+
                 }
             }
 
